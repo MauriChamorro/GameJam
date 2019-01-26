dab737e7a1169d03c8ea4a4d013f6ab7f4695485
Mauri Chamorro <mmchamoo@gmail.com>
2019-01-26 17:54:45 -0300

    first tile map

---
+++ Assets/Scripts/DeadCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DeadCam : MonoBehaviour
+    {
+        public Transform target;
+        public float rLimit;
+        public float lLimit;
+
+        private void Start()
+        {
+            transform.position = target.position;
+        }
+
+        private void Update()
+        {
+            if (true)
+            {
+
+            }
+
+            Vector3 newPos = new Vector3(0, target.position.y, target.position.z);
+        }
+    }
+}
